01ece03c78f591299e15e6cb2c0e2afb76dbb4ed
Daniel Dabrowski <agrcom@yahoo.pl>
2018-08-13 22:33:59 +0200

    initial project files

---
+++ src/main/java/com/daniel/dabrowski/axonlibrary/AxonLibraryApplication.java
@@ -0,0 +1,12 @@
+package com.daniel.dabrowski.axonlibrary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AxonLibraryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AxonLibraryApplication.class, args);
+	}
+}
---
+++ src/test/java/com/daniel/dabrowski/axonlibrary/AxonLibraryApplicationTests.java
@@ -0,0 +1,16 @@
+package com.daniel.dabrowski.axonlibrary;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AxonLibraryApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
