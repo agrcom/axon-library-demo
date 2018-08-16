f519f843e61827866140b4839f057fba91f4a0bc
dabrowsd <agrcom@yahoo.pl>
2018-08-16 12:02:36 +0200

    added up version of axon + all args constructors for borrowBook*

--- src/main/java/com/daniel/dabrowski/axonlibrary/commands/BorrowBookCommand.java
+++ src/main/java/com/daniel/dabrowski/axonlibrary/commands/BorrowBookCommand.java
@@ -1,11 +1,14 @@
 package com.daniel.dabrowski.axonlibrary.commands;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import org.axonframework.commandhandling.TargetAggregateIdentifier;
 
-@Data
+@Data @AllArgsConstructor
 public class BorrowBookCommand {
+
     @TargetAggregateIdentifier
     public String id;
     public String bookName;
+
 }
--- src/main/java/com/daniel/dabrowski/axonlibrary/events/BorrowBookEvent.java
+++ src/main/java/com/daniel/dabrowski/axonlibrary/events/BorrowBookEvent.java
@@ -1,8 +1,9 @@
 package com.daniel.dabrowski.axonlibrary.events;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
-@Data
+@Data @AllArgsConstructor
 public class BorrowBookEvent {
     public String id;
     public String bookName;
