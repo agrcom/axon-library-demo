c96204c37da22afabe9c139388ccd410970df0f9
Daniel Dabrowski <agrcom@yahoo.pl>
2018-08-21 22:12:59 +0200

    refactor - rename event class

--- src/main/java/com/daniel/dabrowski/axonlibrary/AxonLibraryApplication.java
+++ src/main/java/com/daniel/dabrowski/axonlibrary/AxonLibraryApplication.java
@@ -1,10 +1,14 @@
 package com.daniel.dabrowski.axonlibrary;
 
+import com.daniel.dabrowski.axonlibrary.commands.AddBookCommand;
+import org.axonframework.commandhandling.CommandBus;
+import org.axonframework.commandhandling.GenericCommandMessage;
 import org.axonframework.eventsourcing.eventstore.EventStorageEngine;
 import org.axonframework.eventsourcing.eventstore.inmemory.InMemoryEventStorageEngine;
 import org.axonframework.spring.config.EnableAxon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.annotation.Bean;
 
 @EnableAxon
@@ -12,7 +16,9 @@ import org.springframework.context.annotation.Bean;
 public class AxonLibraryApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AxonLibraryApplication.class, args);
+		ConfigurableApplicationContext config = SpringApplication.run(AxonLibraryApplication.class, args);
+		CommandBus commandBus = config.getBean(CommandBus.class);
+		commandBus.dispatch(GenericCommandMessage.asCommandMessage(new AddBookCommand("1", "Clean Code")));
 	}
 
 	@Bean
--- src/main/java/com/daniel/dabrowski/axonlibrary/BookAggregate.java
+++ src/main/java/com/daniel/dabrowski/axonlibrary/BookAggregate.java
@@ -2,8 +2,8 @@ package com.daniel.dabrowski.axonlibrary;
 
 import com.daniel.dabrowski.axonlibrary.commands.AddBookCommand;
 import com.daniel.dabrowski.axonlibrary.commands.BorrowBookCommand;
-import com.daniel.dabrowski.axonlibrary.events.AddBookEvent;
-import com.daniel.dabrowski.axonlibrary.events.BorrowBookEvent;
+import com.daniel.dabrowski.axonlibrary.events.AddedBookEvent;
+import com.daniel.dabrowski.axonlibrary.events.BookBorrowedEvent;
 import lombok.NoArgsConstructor;
 import org.axonframework.commandhandling.CommandHandler;
 import org.axonframework.commandhandling.model.AggregateIdentifier;
@@ -22,26 +22,26 @@ public class BookAggregate {
 
     @CommandHandler
     public BookAggregate(AddBookCommand command) {
-        apply(new AddBookEvent(command.getId(), command.getBookName()));
+        apply(new AddedBookEvent(command.getId(), command.getBookName()));
     }
 
     @CommandHandler
     public void handle(BorrowBookCommand command) throws LibraryIsEmptyExpection {
         if(balance>0) {
-            apply(new BorrowBookEvent(bookId, command.getBookName(), balance));
+            apply(new BookBorrowedEvent(bookId, command.getBookName(), balance));
         }else {
             throw new LibraryIsEmptyExpection();
         }
     }
 
     @EventSourcingHandler
-    public void on(AddBookEvent event){
+    public void on(AddedBookEvent event){
         this.bookId = event.getId();
         this.balance =+ 1;
     }
 
     @EventSourcingHandler
-    public void on(BorrowBookEvent event){
+    public void on(BookBorrowedEvent event){
         this.balance = this.balance - 1;
         event.setBalance(this.balance);
     }
