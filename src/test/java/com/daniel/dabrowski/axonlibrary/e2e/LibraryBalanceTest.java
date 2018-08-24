ba7fe6c0a3aa82ebf645abd614b36e7314c54959
dabrowsd <agrcom@yahoo.pl>
2018-08-24 13:27:21 +0200

    TDD: designed tests for API

--- src/test/java/com/daniel/dabrowski/axonlibrary/e2e/LibraryBalanceTest.java
+++ src/test/java/com/daniel/dabrowski/axonlibrary/e2e/LibraryBalanceTest.java
@@ -2,16 +2,25 @@ package com.daniel.dabrowski.axonlibrary.e2e;
 
 
 import groovy.json.internal.LazyMap;
+import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
 import org.apache.http.HttpStatus;
+import org.junit.Before;
 import org.junit.Test;
 
 import static io.restassured.RestAssured.given;
 
 public class LibraryBalanceTest {
 
+    //@formatter:off
+    @Before
+    public void setup() {
+        RestAssured.baseURI = "localhost";
+        RestAssured.port = 8080;
+    }
+
     @Test
-    public void addBookAndBorrowIt() {
+    public void addBookToShelf() {
         LazyMap basic = new LazyMap();
         basic.put("id", "1");
         basic.put("bookName", "Test Book");
@@ -20,8 +29,33 @@ public class LibraryBalanceTest {
             .body(basic)
             .contentType(ContentType.JSON)
         .when()
-            .post("")
+            .post("/shelf/addBook")
         .then()
             .statusCode(HttpStatus.SC_OK);
     }
+
+    @Test
+    public void borrowBook() {
+        LazyMap basic = new LazyMap();
+        basic.put("id", "1");
+        basic.put("bookName", "Test Book");
+
+        given()
+            .body(basic)
+            .contentType(ContentType.JSON)
+        .when()
+            .post("/shelf/addBook")
+        .then()
+            .statusCode(HttpStatus.SC_OK);
+
+        given()
+            .body(basic)
+            .contentType(ContentType.JSON)
+                .when()
+        .post("/shelf/borrowBook")
+                .then()
+        .statusCode(HttpStatus.SC_OK);
+
+    }
+    //@formatter:on
 }
